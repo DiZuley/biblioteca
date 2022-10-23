4457c1926329c76467a0e9db1cbc823a14fdec2a
DiZuley <116473519+DiZuley@users.noreply.github.com>
2022-10-22 21:03:02 -0600

    guardar y mostrar libros

---
+++ inicio.java
@@ -0,0 +1,11 @@
+
+package Formularios;
+
+
+public class inicio {
+    public static void main(String[] args) {
+        Arreglo_Objetos a=new Arreglo_Objetos();
+        a.setVisible(true);
+    }
+           
+}
